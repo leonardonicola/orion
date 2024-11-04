5a2419683774913a87e7adc7d27c63264995606d
Leonardo Nicola <leonardonicolares@gmail.com>
2024-11-04 14:43:33 -0300

    feat(abort): add AbortController to login form example

--- src/features/auth/ui/LoginForm.vue
+++ src/features/auth/ui/LoginForm.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-  import { reactive } from 'vue'
+  import { onBeforeUnmount, reactive } from 'vue'
   import { useAuthStore } from '../model'
   import { useRouter } from 'vue-router'
 
   const router = useRouter()
   const authStore = useAuthStore()
+  const controller = new AbortController()
 
   const form = reactive({
     email: '',
@@ -12,19 +13,29 @@
   })
 
   const handleSubmit = async () => {
-    const success = await authStore.login(form)
+    const success = await authStore.login(form, controller.signal)
     if (success) {
       router.push('/dashboard')
     }
   }
+  onBeforeUnmount(() => controller.abort())
 </script>
 <template>
-  <form @submit.prevent="handleSubmit">
-    <input v-model="form.email" type="email" required />
-    <input v-model="form.password" type="password" required />
+  <form @submit.prevent="handleSubmit" class="flex w-1/3 flex-col gap-4">
+    <input v-model="form.email" type="email" required class="input" />
+    <input v-model="form.password" type="password" required class="input" />
 
-    <button type="submit" :disabled="authStore.isLoading">
+    <button
+      type="submit"
+      :disabled="authStore.isLoading"
+      class="size-fit rounded-xl border border-gray-100 px-4 py-2 transition-colors hover:border-gray-500"
+    >
       {{ authStore.isLoading ? 'Logging in...' : 'Login' }}
     </button>
   </form>
 </template>
+<style lang="postcss" scoped>
+  .input {
+    @apply h-8 w-full rounded-lg border border-gray-200 p-2 outline-0 transition-colors focus:border-gray-500;
+  }
+</style>
